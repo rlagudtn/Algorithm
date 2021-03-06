c475008d8108e7a7f85bc21b7df7785304e65a4e
rlagudtn <rlagudtn4510@naver.com>
2021-03-07 00:01:57 +0900

    GREEDY #1541 #1946 ##DATE#21.03.07

--- Greedy.py
+++ Greedy.py
@@ -46,8 +46,80 @@
 #         result+=1
 # print(result)
 # # 개수를 출력한다
-###########################1541########################
-###########################1541########################
+
+
+# 2. 플러스 만큼 숫자 반복하며 숫자 더해줌
+# ###########################1541########################
+# string=input()
+# nums=[]
+# signs=[]
+# temp=''
+# length=len(string)
+# for i in range(length):
+#     if i>=length-1:
+#         temp+=string[i]
+#         nums.append(int(temp))
+#     if string[i].isnumeric():
+#         temp+=string[i];
+#     else:
+#         signs.append(string[i])
+#         nums.append(int(temp))
+#         temp = ""
+# def get_result():
+#     temp_s = nums[0]
+#     size = len(signs)
+#     data=[]
+#     for i in range(size+1):
+#         if i>=size:
+#             data.append(temp_s)
+#             break
+#         if signs[i] == '-':
+#             data.append(temp_s)
+#             temp_s=nums[i+1]
+#         elif signs[i] == '+':
+#             temp_s += nums[i+1]
+#     size_d=len(data)
+#     result=data[0]
+#     for i in range(1,size_d):
+#         result-=data[i]
+#     return result
+# print(get_result())
+list=['2','3']
+e=[ sum(list(map(int,x.split('+')))) for x in input().split('-')]
+print(e[0]-sum(e[1:]))
 ###########################11047########################
+# import sys
+# input=sys.stdin.readline
+# n,k=map(int,input().split())
+# coins=[int(input()) for _ in range(n)]
+# # print(coins)
+# coins.sort(reverse=True)
+# result=0
+#
+# for i in coins:
+#
+#     result+=k//i
+#     k=k%i
+# print(result)
+
+###########################1946########################
+import sys
+input=sys.stdin.readline
+
+t=int(input())
+
+for _ in range(t):
+    n=int(input())
+    s=[0]*(n+1)
+    for i in range(n):
+        a,b=map(int,input().split())       ####튜플을 만들지 않고 하는 방법 tech
+        s[a]=b
+    min_n=s[1]
+    count=0 #탈락인원
+    for i in range(2,n+1):
+        if s[i]>min_n: #탈락하는 경우
+            count+=1
+        else: min_n=s[i]
+    print(n-count)
 ###########################11000########################
 
